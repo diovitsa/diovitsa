31c082db37d211382a44ec86d32a3b8916789f6d
diovitsa <dimaiovitsa@gmail.com>
2019-04-03 12:11:40 +0300

    Updates

---
+++ precache-manifest.8cb58d94f41acb260b6742a5a2938de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d16eb7b3f207702faf7f",
+    "url": "/diovitsa/static/js/runtime~main.b8720481.js"
+  },
+  {
+    "revision": "cb4a9a612a2e47c4ddc5",
+    "url": "/diovitsa/static/js/main.c954f63f.chunk.js"
+  },
+  {
+    "revision": "4065b3dcb140a1978325",
+    "url": "/diovitsa/static/js/2.d727c58d.chunk.js"
+  },
+  {
+    "revision": "cb4a9a612a2e47c4ddc5",
+    "url": "/diovitsa/static/css/main.22b50757.chunk.css"
+  },
+  {
+    "revision": "7d0a420b4989cdfb56e8b29da7a06a29",
+    "url": "/diovitsa/index.html"
+  }
+];
